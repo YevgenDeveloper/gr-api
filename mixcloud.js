61680c944be21ce13500c4d3bc4bbb2fe312e8b0
Yevgen <eugen.developer.it@gmail.com>
2022-01-31 07:56:38 +0000

    mixcloud: adding a route to fetch mix infos

--- mixcloud.js
+++ mixcloud.js
@@ -3,7 +3,24 @@ const axios = require('axios');
 const router = express.Router();
 router.get('/', async (req, res) => {
   const link = `https:
-  const frame = await axios.get(link);
-  res.send(frame.data);
+  try {
+    const frame = await axios.get(link);
+    res.send(frame.data);
+  } catch {
+    res.status(404).json({
+      error: 'not found',
+    });
+  }
+});
+router.get('/:show', async (req, res) => {
+  const link = `https:
+  try {
+    const frame = await axios.get(link);
+    res.send(frame.data);
+  } catch {
+    res.status(404).json({
+      error: 'not found',
+    });
+  }
 });
 module.exports = router;
