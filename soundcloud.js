cdda600ddcc9c93d18d657f2da227470ee081c07
Yevgen <eugen.developer.it@gmail.com>
2022-03-16 07:48:17 +0000

    soundcloud: track path

--- soundcloud.js
+++ soundcloud.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const client = process.env.SND_ID;
+const client = `?client_id=${process.env.SND_ID}`;
 const api = `https:
-router.get('/', async (req, res) => {
-  const link = `${api}&limit=20&offset=0&app_locale=fr`;
+const user = `${api}users/713729887/tracks${client}`;
+router.get('/:offset', async (req, res) => {
+  const link = `${user}&limit=20&offset=${req.params.offset}`;
   try {
     const sounds = await axios.get(link);
     res.send(sounds.data);
+  } catch {
+    res.status(404).json({
+      error: 'not found',
+    });
+  }
+});
+router.get('/tracks/:id', async (req, res) => {
+  const link = `${api}tracks/${req.params.id}${client}`;
+  try {
+    const track = await axios.get(link);
+    res.send(track.data);
   } catch {
     res.status(404).json({
       error: 'not found',
