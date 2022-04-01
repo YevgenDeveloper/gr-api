c60dabce3121f87b62f24327a00fdcfbfeee558a
Yevgen <eugen.developer.it@gmail.com>
2022-04-01 06:16:24 +0000

    graph: now able to process pics

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY package*.json ./
 RUN npm install
 COPY *.js ./
 COPY uploads ./
+COPY layouts ./
 CMD [ "node", "index"]
--- fileUpload.js
+++ fileUpload.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const shell = require('shelljs');
 const jwtcheck = require('./jwtcheck');
 const pool = require('./pool');
 const storage = multer.diskStorage({
@@ -16,7 +17,41 @@ const storage = multer.diskStorage({
     cb(null, `${req.body.id}.${split[split.length - 1]}`);
   },
 });
+const insta_storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, '/tmp');
+  },
+  filename: function(req, file, cb) {
+    cb(null, 'insta_pic');
+  },
+});
 const upload = multer({storage: storage});
+const upload_insta = multer({storage: insta_storage});
+router.post(
+  '/insta',
+  async (req, res, next) => {
+    const jwt = await jwtcheck({req});
+    if (jwt.authenticated && jwt.user.role == 'admin') {
+      next();
+    } else res.status(400).json({error: 'you have no rights to upload'});
+  },
+  upload_insta.single('image'),
+  (req, res) => {
+    const date = req.body.date;
+    const month = req.body.month;
+    const title = req.body.title;
+    const artist = req.body.artist;
+    const color = req.body.color;
+    shell.exec(
+      `cd layouts && ./layout.sh /tmp/insta_pic ${date} ${month} "${artist}" "${title}" ${color}`,
+      function(code, stdout, stderr) {
+        if (code != 0)
+          res.status(400).json({error: 'problem converting the picture'});
+      },
+    );
+    res.status(201).sendFile('/tmp/output.png');
+  },
+);
 router.post(
   '/',
   async (req, res, next) => {
@@ -43,8 +78,12 @@ router.put(
     res.status(201).json({data: req.body.id});
   },
 );
-router.get('/:id', (req, res, next) => {
-  res.header('Cache-Control', `public, max-age=${3600 * 24 * 2}`);
-  next();
-}, express.static('./uploads'));
+router.get(
+  '/:id',
+  (req, res, next) => {
+    res.header('Cache-Control', `public, max-age=${3600 * 24 * 2}`);
+    next();
+  },
+  express.static('./uploads'),
+);
 module.exports = router;
