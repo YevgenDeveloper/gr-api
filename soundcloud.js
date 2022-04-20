8d83fca9b416be590a3160b7a1639e4e45558848
Yevgen <eugen.developer.it@gmail.com>
2022-04-20 23:35:25 +0000

    soundcloud: now with search query

--- soundcloud.js
+++ soundcloud.js
@@ -1,28 +1,41 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 const client = `?client_id=${process.env.SND_ID}`;
 const api = `https:
 const user = `${api}users/713729887/tracks${client}`;
-router.get('/:offset', async (req, res) => {
+router.use(express.json());
+router.get("/:offset", async (req, res) => {
   const link = `${user}&limit=12&offset=${req.params.offset}`;
   try {
     const sounds = await axios.get(link);
     res.send(sounds.data);
   } catch {
     res.status(404).json({
-      error: 'not found',
+      error: "not found"
     });
   }
 });
-router.get('/tracks/:id', async (req, res) => {
+router.get("/tracks/:id", async (req, res) => {
   const link = `${api}tracks/${req.params.id}${client}`;
   try {
     const track = await axios.get(link);
     res.send(track.data);
   } catch {
     res.status(404).json({
-      error: 'not found',
+      error: "not found"
+    });
+  }
+});
+router.post("/search", async (req, res) => {
+  console.log(req.body);
+  const link = `${api}search?q=odc-live%20${req.body.search}&client_id=${process.env.SND_ID}&limit=20&offset=${req.body.offset}`;
+  try {
+    const track = await axios.get(link);
+    res.send(track.data);
+  } catch {
+    res.status(403).json({
+      error: "not found"
     });
   }
 });
