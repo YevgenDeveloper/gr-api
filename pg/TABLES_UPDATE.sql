59220d1b311beb9734c9babde2e41fa7f9b9b876
Yevgen <eugen.developer.it@gmail.com>
2022-09-28 09:20:02 +0000

    api: updated PG tables update to change redundancy in shows

--- pg/TABLES_UPDATE.sql
+++ pg/TABLES_UPDATE.sql
@@ -1,2 +1,4 @@
 ALTER TABLE Chat
 DROP COLUMN IF EXISTS ip;
+ALTER TABLE Shows DROP constraint shows_redundancy_check;
+ALTER TABLE Shows ADD constraint shows_redundancy_check check(redundancy >= 0 AND redundancy <= 12);
